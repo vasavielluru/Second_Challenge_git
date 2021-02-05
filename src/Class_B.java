6cf5a3722d115fa423fe952e7cb5688219a64c33
vasavi elluru <elluruvasavi.19993@gmail.com>
2021-02-05 06:28:36 +0530

    some files are deleted

--- src/Class_B.java
+++
@@ -1,9 +0,0 @@
-
-public class Class_B {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("welcome to ClassB");
-	}
-
-}
--- src/Class_C.java
+++
@@ -1,9 +0,0 @@
-
-public class Class_C {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("welcome to ClassC");
-	}
-
-}
